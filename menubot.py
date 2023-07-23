2e1b992f27aed83b43c54f2718efc7f7621f563e
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-22 21:48:42 -0300

    Update menubot.py

--- menubot.py
+++ menubot.py
@@ -24,7 +24,8 @@ async def BrookhavenPlayers(ctx):
                 players_element = soup.find('p', {'class': 'text-lead font-caption-body wait-for-i18n-format-render'})
                 players_count = players_element.text.strip()
                 await ctx.reply(f"The current number of players playing Brookhaven is: {players_count}")
-
+            else:
+                await ctx.reply("Unable to get the number of players at the moment.")
 
 @bot.command()
 async def TSBPlayers(ctx):
