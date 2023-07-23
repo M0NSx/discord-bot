76fd41fe807eac2bdc197647f4fa57460e3e7653
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-22 21:40:39 -0300

    Update menubot.py

--- menubot.py
+++ menubot.py
@@ -46,6 +46,7 @@ async def BloxFruitsPlayers(ctx):
     url = "https://www.roblox.com/games/2753915549/Blox-Fruits"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
+            if resp.status == 200:
 
 
 @bot.command()
